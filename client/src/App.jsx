7c7ecd85b97ea5820bb37243745533696900accc
Davit Grdzelishvili <166071024+davita101@users.noreply.github.com>
2025-03-26 20:59:08 +0400

    add: costum spacing, radius and shadow

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 export default function App() {
     return (
-        <div>
+        <div className="h-screen bg-[#f5f7fa] w-screen text-prenet grid-sidebar trakin">
+            <div className="size-86 bg-white mx-auto shadow-sm rounded-5xl "></div>
         </div>
     );
 }
