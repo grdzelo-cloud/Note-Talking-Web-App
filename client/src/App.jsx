b7f0e1107115a7a384a07e85673969b328a817f3
Davit Grdzelishvili <166071024+davita101@users.noreply.github.com>
2025-03-27 15:56:27 +0400

    fix: styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
     return (
         <div className="h-screen bg-[#f5f7fa] w-screen text-prenet grid-sidebar tracking text-base/[120%]">
-            <div className="size-86 bg-white mx-auto shadow-sm rounded-5xl ">
+            <div className="size-86 text-presnet-1 text-xl font-source-code-pro bg-white mx-auto shadow-sm rounded-5xl ">
                 <p className="text-present-1-secondary-bold">The quick brown fox jumps over the lazy dog.</p>
             </div>
         </div>
