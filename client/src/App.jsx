cd2fb1e12a5efa544c3d2b6fd1e23508f15bf2c2
The1stRule <nikoloz0812@gmail.com>
2025-03-27 12:14:26 +0400

    add: typography

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 export default function App() {
     return (
-        <div className="h-screen bg-[#f5f7fa] w-screen text-prenet grid-sidebar trakin">
-            <div className="size-86 bg-white mx-auto shadow-sm rounded-5xl "></div>
+        <div className="h-screen bg-[#f5f7fa] w-screen text-prenet grid-sidebar tracking text-base/[120%]">
+            <div className="size-86 bg-white mx-auto shadow-sm rounded-5xl ">
+                <p className="text-present-1-secondary-bold">The quick brown fox jumps over the lazy dog.</p>
+            </div>
         </div>
     );
 }
