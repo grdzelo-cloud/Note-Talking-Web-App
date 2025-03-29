3ca7e5e9a9789a29980e136933bf3f22c80869c4
Davit Grdzelishvili <166071024+davita101@users.noreply.github.com>
2025-03-29 04:14:41 +0400

    finish: button, start: modal

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import {Button} from "@/components/ui/button.jsx";
+import Modal from "@/components/ui/modal.jsx";
 
 
 export default function App() {
     return (
-        <div className="h-screen bg-[#f5f7fa] w-screen text-prenet grid-sidebar tracking text-base/[120%]">
+        <div className="h-screen bg-[#f5f7fa] w-screen grid-sidebar">
             <div className="size-86 text-present-1 bg-white mx-auto shadow-sm rounded-5xl ">
-                <p>The quick brown fox jumps over the lazy dog.</p>
-                <Button variant={"border"} disabled>Primary Button</Button>
+                <p className="text-present-4">The quick brown fox jumps over the lazy dog.</p>
+                <Button variant={"border"}>Secondary Button</Button>
+                <Modal/>
             </div>
         </div>
     );
--- client/src/components/ui/button.jsx
+++ client/src/components/ui/button.jsx
@@ -1,14 +1,17 @@
-import { clsx } from 'clsx';
+import {clsx} from 'clsx';
+import {cn} from "@/hooks/cn.js";
 
-export const Button = ({ disabled, children, variant, className }) => {
+export const Button = ({disabled, children, variant = "primary", className}) => {
     return (
         <button
             disabled={disabled}
-            className={clsx(
+            className={cn(
                 "disabled:bg-slate-100 disabled:text-slate-300 disabled:border-slate-100 disabled:cursor-not-allowed",
-                "transition-all duration-100 px-[16px] py-[12px] border-2 focus:outline-offset-2 rounded-lg focus:outline-2  cursor-pointer bg-blue-500 hover:bg-blue-700  text-xs text-present-4", {
-                'focus:outline-offset-2 focus:outline-2 focus:outline-neutral-400 focus:border-neutral-950 hover:border-2  border-neutral-300 hover:border-neutral-100 hover:bg-neutral-100 bg-white hover:text-neutral-600 text-neutral-950 ': variant === 'border',
-                'focus:text-neutral-950 focus:bg-white focus:outline-offset-2 focus:outline-2 focus:outline-neutral-400 hover:text-neutral-950 focus:border-neutral-950 hover:border-2 hover:border-neutral-300 border-neutral-100 bg-neutral-100 hover:bg-white  text-neutral-600 ': variant === 'secondary',
-            }, className)}
+                "transition-all duration-100 px-[16px] py-[12px] border-2 focus:outline-offset-2 rounded-lg focus:outline-[8px] cursor-pointer text-xs font-medium", {
+                    'focus:outline-offset-2 focus:outline-2 focus:outline-neutral-400 focus:border-neutral-950 border-neutral-300 hover:border-neutral-0 hover:bg-neutral-100 bg-white hover:text-neutral-600 text-neutral-950 ': variant === 'border',
+                    'focus:text-neutral-950 focus:bg-white focus:outline-offset-2 focus:outline-2 focus:outline-neutral-400 text-neutral-400 hover:text-neutral-950 focus:border-neutral-950 hover:border-2 hover:border-neutral-300 bg-neutral-100 border-2 border-neutral-100 hover:bg-neutral-0': variant === 'secondary',
+                    'focus:text-white focus:bg-blue-600 bg-blue-500 focus:outline-offset-2 focus:outline-2 focus:outline-neutral-400 hover:bg-blue-700 border-neutral-0 text-present-4 text-neutral-0 font-medium': variant === 'primary',
+                }, className,
+            )}
         >{children}</button>);
 }
